f642e70efa6fdd471688b5f6db3afd7a5381e0ee
mattiasclaesson <mattiaserikanna@gmail.com>
2014-12-08 19:49:28 +0100

    Revert workaround for combi flash fault.

--- TrionicCANFlasher/frmMain.cs
+++ TrionicCANFlasher/frmMain.cs
@@ -90,11 +90,11 @@ namespace TrionicCANFlasher
                     {
                         if (cbxEcuType.SelectedIndex == (int)ECU.TRIONIC7)
                         {
-                            SetOptions(trionic7, false);
+                            SetOptions(trionic7, true);
 
                             AddLogItem("Opening connection");
                             EnableUserInput(false);
-                            if (trionic7.openDevice(false, false))
+                            if (trionic7.openDevice(false, true))
                             {
                                 Thread.Sleep(1000);
                                 AddLogItem("Update FLASH content");
