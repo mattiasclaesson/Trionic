9a6db5ec0753511a4ddc2e59c227baf9a9fe9cf8
mattiasclaesson <mattiaserikanna@gmail.com>
2015-02-12 01:02:29 +0100

    New versions. Upgraded to VS2010

--- SetupCANFlash/version.bat
+++ SetupCANFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set TrionicCANFlasher.version=0.0.22.0
-set TrionicCANLib.version=0.0.21.0
-set SetupCANFlash.version=0.0.22
+set TrionicCANFlasher.version=0.0.23.0
+set TrionicCANLib.version=0.0.22.0
+set SetupCANFlash.version=0.0.23
