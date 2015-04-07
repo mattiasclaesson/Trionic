208dccbcc986c3cd30a47964e1c3ffb78c1d9807
mattiasclaesson <mattiaserikanna@gmail.com>
2015-04-07 23:28:44 +0200

    Fixed missing dll.

--- SetupCANFlash/version.bat
+++ SetupCANFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set TrionicCANFlasher.version=0.1.2.0
-set TrionicCANLib.version=0.1.2.0
-set SetupCANFlash.version=0.1.2
+set TrionicCANFlasher.version=0.1.4.0
+set TrionicCANLib.version=0.1.4.0
+set SetupCANFlash.version=0.1.4
