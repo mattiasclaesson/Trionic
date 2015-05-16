04d8a415432ebf7a52967b2d75350eb704133021
mattiasclaesson <mattiaserikanna@gmail.com>
2015-05-16 19:30:00 +0200

    Changed release script

--- release.bat
+++ release.bat
@@ -4,6 +4,6 @@ mkdir C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%
 xcopy SetupCANFlash\version.bat C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
 xcopy SetupCANFlash\Release\TrionicCANFlash.msi C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
 
+git tag SetupCANFlash_v%SetupCANFlash.version%
 git tag TrionicCanFlasher_v%TrionicCANFlasher.version%
 git tag TrionicCANLib_v%TrionicCANLib.version%
-git tag SetupCANFlash_v%SetupCANFlash.version%
