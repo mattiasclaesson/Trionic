c6c3c1955228bb31e2a474379ff75fd265472ef9
mattiasclaesson <mattiaserikanna@gmail.com>
2015-07-22 12:05:54 +0200

    Fixed mistake in release script for md5

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@ call SetupCANFlash\version.bat
 devenv Trionic.sln /Rebuild Release /project SetupCANFlash
 
 pushd SetupCANFlash\Release\
-C:\GitHub\Trionic>"C:\Program Files (x86)\hashutils-1.3.0-redist\bin.x86-32\md5sum.exe" TrionicCANFlash.msi >> TrionicCANFlash.md5
+"C:\Program Files (x86)\hashutils-1.3.0-redist\bin.x86-32\md5sum.exe" TrionicCANFlash.msi >> TrionicCANFlash.md5
 popd
 
 mkdir C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%
