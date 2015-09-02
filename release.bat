07fe5d6868c93cd80ae0d4424955e4edc996709f
mattiasclaesson <mattiaserikanna@gmail.com>
2015-09-02 23:32:15 +0200

    No more dropbox

--- release.bat
+++ release.bat
@@ -6,20 +6,21 @@ pushd SetupCANFlash\Release\
 "C:\Program Files\7-Zip\7z.exe" a -tzip TrionicCANFlash.zip TrionicCANFlash.* setup.exe
 popd
 
-mkdir C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%
-xcopy SetupCANFlash\version.bat C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
-xcopy SetupCANFlash\Release\TrionicCANFlash.msi C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
-xcopy SetupCANFlash\Release\TrionicCANFlash.md5 C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
-xcopy SetupCANFlash\Release\TrionicCANFlash.zip C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
-xcopy SetupCANFlash\Release\setup.exe C:\users\mattias\Dropbox\public\TrionicCANFlasher\%SetupCANFlash.version%\
+mkdir C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%
+xcopy SetupCANFlash\version.bat C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%\
+xcopy SetupCANFlash\Release\TrionicCANFlash.msi C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%\
+xcopy SetupCANFlash\Release\TrionicCANFlash.md5 C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%\
+xcopy SetupCANFlash\Release\TrionicCANFlash.zip C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%\
+xcopy SetupCANFlash\Release\setup.exe C:\users\mattias\Delivery\TrionicCANFlasher\%SetupCANFlash.version%\
 
 
-echo ^<?xml version="1.0" encoding="utf-8"?^>  > C:\users\mattias\Dropbox\public\TrionicCANFlasher\version.xml
-echo ^<canflasher version="%SetupCANFlash.version%"/^> >> C:\users\mattias\Dropbox\public\TrionicCANFlasher\version.xml
+echo ^<?xml version="1.0" encoding="utf-8"?^>  > C:\users\mattias\Delivery\TrionicCANFlasher\version.xml
+echo ^<canflasher version="%SetupCANFlash.version%"/^> >> C:\users\mattias\Delivery\TrionicCANFlasher\version.xml
 
 echo ----------------------------------------------------
 git changes
 echo ----------------------------------------------------
+
 git tag SetupCANFlash_v%SetupCANFlash.version%
 git tag TrionicCanFlasher_v%TrionicCANFlasher.version%
 git tag TrionicCANLib_v%TrionicCANLib.version%
