0fd24168980ab1843db547f1d4fbcb188c29cc8b
mattiasclaesson <mattiaserikanna@gmail.com>
2016-11-15 21:07:27 +0100

    New version

--- SetupCANFlash/version.bat
+++ SetupCANFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set TrionicCANFlasher.version=0.1.43.0
-set TrionicCANLib.version=0.1.32.0
-set SetupCANFlash.version=0.1.43
+set TrionicCANFlasher.version=0.1.44.0
+set TrionicCANLib.version=0.1.33.0
+set SetupCANFlash.version=0.1.44
