3c988d032c379202852ad7ff739f5c2380b83eba
Christian Ivarsson <ivarsson.itk@gmail.com>
2018-02-14 11:57:55 +0100

    Trionic5, checksum; Use header data to find last address
    It is still not perfect but I'd rather do it this way than how the
    manual says it should be done due to problems with Trionic 5.2

--- TrionicCANLib/Checksum/ChecksumT5.cs
+++ TrionicCANLib/Checksum/ChecksumT5.cs
@@ -10,108 +10,125 @@ namespace TrionicCANLib.Checksum
     public class ChecksumT5
     {
         private readonly static Logger logger = LogManager.GetCurrentClassLogger();
-
-        private static byte[,] T5markers = new byte[2, 4]
-        {
-            {0x19, 0x0A, 0x4E, 0xF9}, // T5.2 marker (Scan from 1FF90 and down)
-            {0x4E, 0xFA, 0xFB, 0xCC}  // T5.5 marker (Scan from 3FF90 and down)
-        };
-
-        // Trionic 5.2 markers in different bins:
-        // 19 0A 4E F9 00 07 EA 90
-        // 19 0A 4E F9 00 07 E9 EA
-        // 19 0A 4E F9 00 07 ED 2A
-
+        
         /// <summary>
-        /// Validates a binary before flashing
+        /// Validates a binary before flashing.
         /// </summary>
         /// <param name="filename">filename to flash</param>
         /// <returns>ChecksumResult.Ok if checksum is correct</returns>
         public static ChecksumResult VerifyChecksum(string filename)
         {
             FileInfo fi = new FileInfo(filename);
-            long len    = fi.Length;
+            long len = fi.Length;
 
-            // Verify file length
+            // Verify file length.
             if (len != FileT5.LengthT52 && len != FileT5.LengthT55)
-            {
                 return ChecksumResult.InvalidFileLength;
-            }
 
-            byte[] Localbuffer = FileTools.readdatafromfile(filename, 0, (int) len);
+            // Store file in a local buffer.
+            byte[] Bufr = FileTools.readdatafromfile(filename, 0, (int)len);
 
-            uint ReadSum = (uint)(
-                Localbuffer[len - 4] << 24 | Localbuffer[len - 3] << 16 |
-                Localbuffer[len - 2] <<  8 | Localbuffer[len - 1]);
-
-            logger.Debug("Read checksum: " + ReadSum.ToString("X8"));
-            logger.Debug("Determining index by size");
-            if (!ChecksumMatch(Localbuffer, ReadSum, FindEndmarker(Localbuffer, (byte)(len>>18))))
-            {
-                logger.Debug("Trying doubled T5.2");
-                if (!ChecksumMatch(Localbuffer, ReadSum, FindEndmarker(Localbuffer, 0)))
-                {
-                    return ChecksumResult.Invalid;
-                }
-            }
-            
-            return ChecksumResult.Ok;
+            return ChecksumMatch(Bufr, (uint)len) ? ChecksumResult.Ok : ChecksumResult.Invalid;
+        }
+        
+        
+        /// <summary>
+        /// Converts one ASCII character into one nibble.
+        /// </summary>
+        /// <param name="In">One ASCII character</param>
+        /// <returns>One nibble in binary format</returns>
+        private static int ToNibble(char In)
+        {
+            if (In > 0x29 && In < 0x40)          // 0 - 9
+                return In & 0xF;
+            else if ((In > 0x40 && In < 0x47) || // A - F
+                     (In > 0x60 && In < 0x67))   // a - f
+                return (In + 9) & 0xF;
+            else
+                return -1; // Not ASCII!
         }
 
         /// <summary>
-        /// Validates a dumped binary
+        /// Verifies whether a binary has the correct checksum or not.
+        /// </summary>
+        /// <param name="Bufr">A buffered copy of the binary</param>
+        /// <param name="Len">Length of passed buffer</param>
+        /// <returns>Checksum correct: true or false</returns>
+        private static bool ChecksumMatch(byte[] Bufr, uint Len)
+        {
+            uint Loc = Len - 5; // Current location in file.
+            uint End = 0;       // Last used address.  
+
+            // Last four bytes contains a Checksum. Store those for further processing.
+            uint StoredChecksum = (uint)(
+                Bufr[Len - 4] << 24 | Bufr[Len - 3] << 16 |
+                Bufr[Len - 2] <<  8 | Bufr[Len - 1]);
+
+            // Attempt to find the 0xFE container.
+            while (Bufr[Loc - 1] != 0xFE)
+            {
+                Loc -= ((uint)(Bufr[Loc]) + 2);
+
+                // No binary has this little data; Abort!
+                if (Loc < (Len / 2))
+                {
+                    logger.Debug("Could not find container!");
+                    return false;
+                }
+            }
+
+            // Store length of end-marker string.
+            int MrkL = Bufr[Loc];
+
+            // Abort if string is longer than 8 chars.
+            if (MrkL > 8)
+            {
+                logger.Debug("Marker too long!");
+                return false;
+            }
+
+            // Convert ASCII string into usable data.
+            for (int i = MrkL; i > 0; i--)
+            {
+                int Nibble = ToNibble((char)Bufr[(Loc - (MrkL - i)) - 2]);
+
+                // Abort if non-ASCII characters are read!
+                if (Nibble == -1)
+                {
+                    logger.Debug("Read invalid data!");
+                    return false;
+                }
+
+                // Bitshift result into the correct location.
+                End |= (uint)Nibble << ((i - 1) * 4);
+            }
+
+            // Convert from physical address to file address.
+            End -= (0x7FFFF - Len);
+
+            // Do not checksum outside of binary.
+            if (End > Len - 7)
+            {
+                logger.Debug("Pointer outside of binary!");
+                return false;
+            }
+
+            // Subtract byte from the checksum to, hopefully, end up with 0 as the total sum.
+            for (uint i = 0; i < End; i++)
+                StoredChecksum -= (byte)Bufr[i];
+
+            return StoredChecksum == 0 ? true : false;
+        }
+        
+        /// <summary>
+        /// Validates a dumped binary.
         /// </summary>
         /// <param name="Bufr">buffered copy of current binary</param>
         /// <param name="IsT55">true for trionic 5.5, false for 5.2</param>
-        /// <returns>Checksum match true or false</returns>
+        /// <returns>Checksum match: true or false</returns>
         public static bool ValidateDump(byte[] Bufr, bool IsT55)
         {
-            long len = IsT55 ? FileT5.LengthT55 : FileT5.LengthT52;
-            uint ReadSum = (uint)(
-                Bufr[len - 4] << 24 | Bufr[len - 3] << 16 |
-                Bufr[len - 2] <<  8 | Bufr[len - 1]);
-
-            return ChecksumMatch(Bufr, ReadSum, FindEndmarker(Bufr, (byte)(len>>18)));
-        }
-
-        /// <summary>
-        /// Verifies stored checksum against a calculated one
-        /// </summary>
-        /// <param name="Bufr">buffered copy of current binary</param>
-        /// <param name="Sum">calculated checksum</param>
-        /// <param name="End">where to end checksum32 calculations</param>
-        /// <returns>Checksum match true or false</returns>
-        private static bool ChecksumMatch(byte[] Bufr, uint Sum, uint End)
-        {
-            for (uint i = 0; i < End; i++)
-            {
-                Sum -= Bufr[i];
-            }
-
-            return (Sum & 0xFFFFFFFF) == 0 ? true : false;
-        }
-
-        /// <summary>
-        /// Scans binary for an endmarker
-        /// </summary>
-        /// <param name="Bufr">buffered copy of current binary</param>
-        /// <param name="index">1 for trionic 5.5, 0 for 5.2</param>
-        /// <returns>Last address of binary + 1</returns>
-        private static uint FindEndmarker(byte[] Bufr, byte index)
-        {
-            uint len = (index == 1 ? FileT5.LengthT55 : FileT5.LengthT52);
-
-            for (uint i = (len - 112); i > 3; i--)
-            {
-                if (T5markers[index, 3] == Bufr[  i  ] && T5markers[index, 2] == Bufr[i - 1] &&
-                    T5markers[index, 1] == Bufr[i - 2] && T5markers[index, 0] == Bufr[i - 3])
-                {
-                    return i + (uint)(index == 1? 1 : 5);
-                }
-            }
-
-            logger.Debug("NO MARKER !!!");
-            return 0;
+            return ChecksumMatch(Bufr, IsT55 ? FileT5.LengthT55 : FileT5.LengthT52);
         }
     }
 }
