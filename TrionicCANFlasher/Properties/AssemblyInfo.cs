7689ad04f7e41a47ccdcdd0c7d733a493702f83e
mattiasclaesson <mattiaserikanna@gmail.com>
2018-04-06 08:59:51 +0200

    New version

--- TrionicCANFlasher/Properties/AssemblyInfo.cs
+++ TrionicCANFlasher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MattiasC")]
 [assembly: AssemblyProduct("TrionicCANFlasher")]
-[assembly: AssemblyCopyright("Copyright © 2017 Dilemma/Mattias Claesson")]
+[assembly: AssemblyCopyright("Copyright © 2018 Dilemma/Mattias Claesson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.57.0")]
-[assembly: AssemblyFileVersion("0.1.57.0")]
+[assembly: AssemblyVersion("0.1.58.0")]
+[assembly: AssemblyFileVersion("0.1.58.0")]
