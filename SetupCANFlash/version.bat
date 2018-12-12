716e6c9ed3093653089adaccafe2f8fa36d60ab3
mattiasclaesson <mattiaserikanna@gmail.com>
2018-12-12 19:51:04 +0100

    New version, added merge module for vs2010 c++

--- SetupCANFlash/version.bat
+++ SetupCANFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set TrionicCANFlasher.version=0.1.64.0
-set TrionicCANLib.version=0.1.52.0
-set SetupCANFlash.version=0.1.64
+set TrionicCANFlasher.version=0.1.65.0
+set TrionicCANLib.version=0.1.53.0
+set SetupCANFlash.version=0.1.65
