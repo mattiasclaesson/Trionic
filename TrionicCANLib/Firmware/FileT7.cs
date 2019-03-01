c3c0e1de4bc8941ed5cf2b33f2f8b5feace368d4
mattiasclaesson <mattiaserikanna@gmail.com>
2019-03-01 19:23:15 +0100

    SRAM addresses

--- TrionicCANLib/Firmware/FileT7.cs
+++ TrionicCANLib/Firmware/FileT7.cs
@@ -7,6 +7,7 @@ namespace TrionicCANLib.Firmware
     public class FileT7 : BaseFile
     {
         public const uint Length = 0x80000;
+        public const uint SRAMAddress = 0x0F00000;
 
         static public new bool VerifyFileSize(long size)
         {
--- TrionicCANLib/Firmware/FileT8.cs
+++ TrionicCANLib/Firmware/FileT8.cs
@@ -7,6 +7,7 @@ namespace TrionicCANLib.Firmware
     public class FileT8 : BaseFile
     {
         public const uint Length = 0x100000;
+        public const uint SRAMAddress = 0x100000;
 
         static public new bool VerifyFileSize(long size)
         {
