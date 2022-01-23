2ddf1de51bd37f4bbac7af6d98a7c9349affea58
Mattias Claesson <mattiaserikanna@gmail.com>
2022-01-23 14:17:34 +0100

    New canlib for t8suite

--- SetupCANFlash/version.bat
+++ SetupCANFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set TrionicCANFlasher.version=0.1.68.0
-set TrionicCANLib.version=0.1.56.0
-set SetupCANFlash.version=0.1.68
+set TrionicCANFlasher.version=0.1.69.0
+set TrionicCANLib.version=0.1.57.0
+set SetupCANFlash.version=0.1.69
